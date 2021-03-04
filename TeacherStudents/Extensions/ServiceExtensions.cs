d6ef33133e6a671ce1ecaa4165e933f30bb3fa88
Robinson Souza <Robinsond76@gmail.com>
2021-03-04 15:08:00 -0500

    Add project files.

---
+++ TeacherStudents/Extensions/ServiceExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeacherStudents.Extensions
+{
+    public static class ServiceExtensions
+    {
+        //CORS
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader()
+                          .AllowAnyMethod()
+                          .AllowAnyOrigin();
+                });
+            });
+
+        //IIS Integration
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+    }
+}
